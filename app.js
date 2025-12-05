b45d070961867ab681859daf6e9f14dcfc12617e
Tanishq Pratap Singh <singh15tanishq@gmail.com>
2025-12-05 18:10:14 -0500

    Created required files and completed connections in app.js

---
+++ app.js
@@ -0,0 +1,93 @@
+// CPlanify - main app file (app.js)
+//By- Tanishq Pratap Singh (200587638)
+
+//making all the required imports
+require('dotenv').config();
+const express = require('express');
+
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const methodOverride = require('method-override');
+
+const session = require('express-session');
+const flash = require('connect-flash');
+
+
+
+// passport setup for authentication
+const passport = require('./config/passport');
+
+// connect to MongoDB using Mongoose
+require('./config/db');
+
+//Express
+const app = express();
+
+const homeRoutes = require('./routes/index');
+
+const authRoutes = require('./routes/auth');
+
+const eventRoutes = require('./routes/events');
+
+// importing handlebars 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+
+// reading form data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// cookies + method override (for PUT/DELETE)
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+// public folder for css, js, images
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// session setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'cplanify-secret-key',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// flash messages
+app.use(flash());
+
+// passport initilization
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+// setting global variables for templates
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// using the routes
+app.use('/', homeRoutes);
+app.use('/auth', authRoutes);
+app.use('/events', eventRoutes);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).render('error', { message: 'Page not found' });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).render('error', { message: err.message });
+});
+
+module.exports = app;
